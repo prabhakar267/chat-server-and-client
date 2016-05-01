c93968f5e4c624f6d3c870b0df5f96514d381ff8
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-05-02 00:00:00 +0530

    client code updated

--- client.py
+++ client.py
@@ -2,58 +2,54 @@
 # @Author: prabhakar
 # @Date:   2016-04-25 00:15:45
 # @Last Modified by:   Prabhakar Gupta
-# @Last Modified time: 2016-04-25 00:15:50
+# @Last Modified time: 2016-05-04 02:22:32
 
-# telnet program example
-import socket, select, string, sys
- 
+import socket
+import select
+import string
+import sys
+
 def prompt() :
-    sys.stdout.write('<You> ')
-    sys.stdout.flush()
- 
-#main function
+	sys.stdout.write('<You> ')
+	sys.stdout.flush()
+
 if __name__ == "__main__":
-     
-    if(len(sys.argv) < 3) :
-        print 'Usage : python telnet.py hostname port'
-        sys.exit()
-     
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-     
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-     
-    # connect to remote host
-    try :
-        s.connect((host, port))
-    except :
-        print 'Unable to connect'
-        sys.exit()
-     
-    print 'Connected to remote host. Start sending messages'
-    prompt()
-     
-    while 1:
-        socket_list = [sys.stdin, s]
-         
-        # Get the list sockets which are readable
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-         
-        for sock in read_sockets:
-            #incoming message from remote server
-            if sock == s:
-                data = sock.recv(4096)
-                if not data :
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else :
-                    #print data
-                    sys.stdout.write(data)
-                    prompt()
-             
-            #user entered a message
-            else :
-                msg = sys.stdin.readline()
-                s.send(msg)
-                prompt()
+
+	if(len(sys.argv) < 3) :
+		print 'Usage : python telnet.py hostname port'
+		sys.exit()
+
+	host = sys.argv[1]
+	port = int(sys.argv[2])
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(2)
+
+	try :
+		s.connect((host, port))
+	except :
+		print 'Unable to connect'
+		sys.exit()
+
+	print 'Connected to remote host. Start sending messages'
+	prompt()
+
+	while 1:
+		socket_list = [sys.stdin, s]
+
+		read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+
+		for sock in read_sockets:
+			if sock == s:
+				data = sock.recv(4096)
+				if not data :
+					print '\nDisconnected from chat server'
+					sys.exit()
+				else :
+					sys.stdout.write(data)
+					prompt()
+
+			else :
+				msg = sys.stdin.readline()
+				s.send(msg)
+				prompt()
